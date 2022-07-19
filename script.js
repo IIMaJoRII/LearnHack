c411f98119d997cf9ebb0c4b8b0500d3ec239426
IIMaJoRII <kirill.vajnilo12345678@gmail.com>
2022-07-19 16:33:13 +0300

    number 2

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.getElementById("button").onclick = function () {
     alert ("Вы нажали кнопку.");
 }
+
+    function randomString(i) {
+        var rnd = '';
+        while (rnd.length < i) 
+            rnd += Math.random().toString(36).substring(2);
+        return rnd.substring(0, i);
+    };
+
+let button1 = document.getElementById('button1').onclick = function () {
+    let p = document.getElementById("p1");
+    p.innerText = randomString(25);
+}
+
+
